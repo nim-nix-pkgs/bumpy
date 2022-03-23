a6731b7c9fcbd124b286905149685834e99ce110
hugosenari <hugosenari@gmail.com>
2022-03-23 19:07:57 -0300

    feat(nimpkgs): remove lock

--- 1_0_3/flake.nix
+++ 1_0_3/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''2d collision library for Nim.'';
+  description = ''2d collision library.'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -12,6 +12,14 @@
   inputs.src-bumpy-1_0_3.owner = "treeform";
   inputs.src-bumpy-1_0_3.repo  = "bumpy";
   inputs.src-bumpy-1_0_3.type  = "github";
+  
+  inputs."vmath".owner = "nim-nix-pkgs";
+  inputs."vmath".ref   = "master";
+  inputs."vmath".repo  = "vmath";
+  inputs."vmath".dir   = "1_1_2";
+  inputs."vmath".type  = "github";
+  inputs."vmath".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."vmath".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
