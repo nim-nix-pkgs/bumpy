9bade0e40b39b83585a4a31aeb8d7c0f96ae3710
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:16 -0300

    chore: re index bumpy

--- flake.nix
+++ flake.nix
@@ -7,6 +7,78 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."bumpy-master".dir   = "master";
+  inputs."bumpy-master".owner = "nim-nix-pkgs";
+  inputs."bumpy-master".ref   = "master";
+  inputs."bumpy-master".repo  = "bumpy";
+  inputs."bumpy-master".type  = "github";
+  inputs."bumpy-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bumpy-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bumpy-0_1_0".dir   = "0_1_0";
+  inputs."bumpy-0_1_0".owner = "nim-nix-pkgs";
+  inputs."bumpy-0_1_0".ref   = "master";
+  inputs."bumpy-0_1_0".repo  = "bumpy";
+  inputs."bumpy-0_1_0".type  = "github";
+  inputs."bumpy-0_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bumpy-0_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bumpy-0_2_0".dir   = "0_2_0";
+  inputs."bumpy-0_2_0".owner = "nim-nix-pkgs";
+  inputs."bumpy-0_2_0".ref   = "master";
+  inputs."bumpy-0_2_0".repo  = "bumpy";
+  inputs."bumpy-0_2_0".type  = "github";
+  inputs."bumpy-0_2_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bumpy-0_2_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bumpy-0_2_1".dir   = "0_2_1";
+  inputs."bumpy-0_2_1".owner = "nim-nix-pkgs";
+  inputs."bumpy-0_2_1".ref   = "master";
+  inputs."bumpy-0_2_1".repo  = "bumpy";
+  inputs."bumpy-0_2_1".type  = "github";
+  inputs."bumpy-0_2_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bumpy-0_2_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bumpy-1_0_0".dir   = "1_0_0";
+  inputs."bumpy-1_0_0".owner = "nim-nix-pkgs";
+  inputs."bumpy-1_0_0".ref   = "master";
+  inputs."bumpy-1_0_0".repo  = "bumpy";
+  inputs."bumpy-1_0_0".type  = "github";
+  inputs."bumpy-1_0_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bumpy-1_0_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bumpy-1_0_1".dir   = "1_0_1";
+  inputs."bumpy-1_0_1".owner = "nim-nix-pkgs";
+  inputs."bumpy-1_0_1".ref   = "master";
+  inputs."bumpy-1_0_1".repo  = "bumpy";
+  inputs."bumpy-1_0_1".type  = "github";
+  inputs."bumpy-1_0_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bumpy-1_0_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bumpy-1_0_2".dir   = "1_0_2";
+  inputs."bumpy-1_0_2".owner = "nim-nix-pkgs";
+  inputs."bumpy-1_0_2".ref   = "master";
+  inputs."bumpy-1_0_2".repo  = "bumpy";
+  inputs."bumpy-1_0_2".type  = "github";
+  inputs."bumpy-1_0_2".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bumpy-1_0_2".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bumpy-1_0_3".dir   = "1_0_3";
+  inputs."bumpy-1_0_3".owner = "nim-nix-pkgs";
+  inputs."bumpy-1_0_3".ref   = "master";
+  inputs."bumpy-1_0_3".repo  = "bumpy";
+  inputs."bumpy-1_0_3".type  = "github";
+  inputs."bumpy-1_0_3".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bumpy-1_0_3".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."bumpy-1_1_0".dir   = "1_1_0";
+  inputs."bumpy-1_1_0".owner = "nim-nix-pkgs";
+  inputs."bumpy-1_1_0".ref   = "master";
+  inputs."bumpy-1_1_0".repo  = "bumpy";
+  inputs."bumpy-1_1_0".type  = "github";
+  inputs."bumpy-1_1_0".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."bumpy-1_1_0".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
